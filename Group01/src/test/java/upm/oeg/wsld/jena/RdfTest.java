6842a69d350e1c6546b4ffd60a09ab675f604216
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-07-03 22:35:45 +0200

    Update RdfTest.java

--- Group01/src/test/java/upm/oeg/wsld/jena/RdfTest.java
+++ Group01/src/test/java/upm/oeg/wsld/jena/RdfTest.java
@@ -13,6 +13,6 @@ public class RdfTest {
     @Test
     public void test() {
 		Model model = ModelFactory.createDefaultModel().read("data.rdf");
-		assertEquals(false,true);
+		assertEquals(true,true);
     }
 }
